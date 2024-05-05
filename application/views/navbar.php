1d140f3b36f8ac7a5923a5bafee9123b5eacc05f
Geronimo Silvestre <geronimosilvestre@hotmail.com>
2024-05-05 12:11:52 -0300

    nav-bar update

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -13,20 +13,32 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link disabled">Disabled</a>
       </li>
+    </ul>
+
+
+    
+
+    <ul class="nav navbar-nav vanbar-rigth">
+
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+            Dropdown
+            </a>
+            <div class="dropdown-menu">
+            <a class="dropdown-item" href="<?php echo site_url ("user"); ?>">Ingresar</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="<?php echo site_url("user/log_out"); ?>">Log out</a>
+            </div>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo site_url ("user"); ?>"><i class="bi bi-box-arrow-in-right"></i> Ingresar</a>
+        </li>
+      
     </ul>
     
   </div>
