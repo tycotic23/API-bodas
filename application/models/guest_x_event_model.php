070c48f466b3b530997f0118b8c2e76a1fd06ffd
Geronimo Silvestre <geronimosilvestre@hotmail.com>
2024-05-06 13:49:40 -0300

    view guest_x_event, show events foreach guest

--- application/models/guest_x_event_model.php
+++ application/models/guest_x_event_model.php
@@ -19,20 +19,60 @@ Class Guest_x_event_model extends CI_Model {
         return $this->db->get($this->database)->row_array();
     }
 
-    function get_by_guest($guest_id=null){
-        $this->db->where("invitado_id",$guest_id);
-        $this->db->limit(1);
-        return $this->db->get($this->database)->row_array();
+    function get_by_guest($guest_id=null) {
+        if ($guest_id != null) {
+            
+            $this->db->join('eventos', 'eventos.evento_id = invitado_x_evento.evento_id','inner');
+            $this->db->where('invitado_id', $guest_id);
+            $query = $this->db->get("invitado_x_evento");
+            return $query->result_array();
+            
+        } else {
+            return false; 
+        }
     }
 
-        // nueva funcion a avergiuar como funciona.
+
+        
+        /*$this->db->where("invitado_id",$guest_id);
+        $this->db->limit(1);
+        return $this->db->get($this->database)->row_array();*/
+
+   /* // nueva funcion a avergiuar como funciona.
     public function get_eventos_por_usuario($guest_id) {
+        
         $this->db->select('eventos.*');
         $this->db->from('eventos');
         $this->db->join('invitado_x_evento', 'invitado_x_evento.evento_id = eventos.id');
         $this->db->where('invitados_x_eventos.invitado_id', $guest_id);
         return $this->db->get()->result();
-    }
+
+        $this->db->select('eventos.*');
+        $this->db->from('eventos');
+        $this->db->join('invitado_x_evento', 'invitado_x_evento.evento_id = invitado_x_evento.evento_id');
+        $this->db->where('usuario_rol.usuario_id', $usuario_id);
+        $query = $this->db->get();
+        $result = $query->result();
+    //aca ---
+            if($this->rubro_actual){
+                $this->db->where("eventos.evento_id",$guest_id);
+                $this->db->where("invitados.invitado_id",$guest_id);
+                $this->db->order_by("evento_id");
+
+                $this->db->join("articulos_x_rubros","articulos.articulo_id=articulos_x_rubros.articulo_id","inner");
+
+                $this->db->join("rubros","articulos_x_rubros.rubro_id=rubros.rubro_id","inner");
+                return $this->db->get("articulos")->result_array();
+
+                $this->db->select('roles.*');
+                $this->db->from('roles');
+                $this->db->join('usuario_rol', 'roles.id = usuario_rol.rol_id');
+                $this->db->where('usuario_rol.usuario_id', $usuario_id);
+                $query = $this->db->get();
+                $result = $query->result();
+            }
+    //aca ---
+*/
 
     public function create_guest_x_event ($event_id="",$guest_id="",$asist="",$coments=""){
 
